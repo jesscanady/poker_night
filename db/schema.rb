da3e3630084364f6c9dd40504a42d5bacb734cd6
Jon Canady <jon@joncanady.com>
2010-05-29 15:00:03 -0400

    Adding admin flag to users

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100529185421) do
+ActiveRecord::Schema.define(:version => 20100529185919) do
 
   create_table "nights", :force => true do |t|
     t.datetime "when"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20100529185421) do
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
 end
