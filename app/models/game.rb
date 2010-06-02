b748098a727da57a4900ee02cd68d63fa370987e
Jon Canady <jon@joncanady.com>
2010-06-01 23:51:28 -0400

    Convenience methods on Game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,21 @@
 class Game < ActiveRecord::Base
   has_many :records
-  has_many :users, :through => :records
+  has_many :players, :through => :records, :source => :user, :class_name => 'User', :foreign_key => :user_id do 
+    def ranked
+      all(:order => 'place ASC')
+    end
+  end
     
   validates_presence_of :scheduled_for
+  
+  named_scope :active, :conditions => {:active => true}
+  
+  def self.previous
+    active.find(:first, :conditions => ['scheduled_for <= ?', Time.now])
+  end
+  
+  def self.upcoming
+    active.find(:first, :conditions => ['scheduled_for >= ?', Time.now])
+  end
+  
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,38 @@
 require 'spec_helper'
 
 describe Game do
+  
+  context "Announcements" do
+
+    before(:each) do
+      @upcoming_game = Factory(:game, :scheduled_for => Time.now + 1.week, :active => true)
+      @last_game = Factory(:game, :scheduled_for => Time.now - 1.week, :active => true)
+      @older_game = Factory(:game, :scheduled_for => Time.now - 2.weeks, :active => true)
+      @cancelled_game = Factory(:game, :scheduled_for => Time.now - 3.weeks, :active => false)
+    end
+
+    it "should fetch the latest game" do 
+      Game.previous.should == @last_game
+    end
+    
+    it "should fetch the upcoming game" do
+      Game.upcoming.should == @upcoming_game
+    end
+  end
+  
+  context "rankings" do 
+    before(:each) do
+      @last_game = Factory(:game, :scheduled_for => Time.now - 1.week, :active => true)
+      @last_game.records << Factory(:record, :place => 5, :user => Factory(:user, :id => 1), :game => @last_game)
+      @last_game.records << Factory(:record, :place => 4, :user => Factory(:user, :id => 2), :game => @last_game)
+      @last_game.records << Factory(:record, :place => 3, :user => Factory(:user, :id => 3), :game => @last_game)
+      @last_game.records << Factory(:record, :place => 2, :user => Factory(:user, :id => 4), :game => @last_game)
+      @last_game.records << Factory(:record, :place => 1, :user => Factory(:user, :id => 5), :game => @last_game)
+    end
+    
+    it "should rank players easily and properly" do
+      @last_game.players.ranked.map(&:id).should == [5,4,3,2,1]
+    end
+  end
 
 end
