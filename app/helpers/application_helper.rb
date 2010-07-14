c28b287d8c89bc41159762762ae1633d2dc82754
Jon Canady <jon@joncanady.com>
2010-07-14 09:23:53 -0400

    Cheap favicon, Lobster Google Font on h2s, Winner's table rejiggering.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def google_font_link_tag(family)
+    tag('link', {:rel => :stylesheet, :type => Mime::CSS, :href => "http://fonts.googleapis.com/css?family=#{family}"}, false, false)
+  end
+
+  def favicon_link_tag(source='/favicon.ico', options={})
+    tag('link', {
+          :rel  => 'shortcut icon',
+          :type => 'image/vnd.microsoft.icon',
+          :href => path_to_image(source)
+        }.merge(options.symbolize_keys))
+  end
+  
 end
